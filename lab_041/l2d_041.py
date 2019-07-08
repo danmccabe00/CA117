1aa46f53ffe35cab0e665aba075b1bdfc625fedc
danmccabe00 <52660514+danmccabe00@users.noreply.github.com>
2019-07-08 14:00:23 +0100

    Create l2d_041.py

---
+++ lab_041/l2d_041.py
@@ -0,0 +1,10 @@
+def l2d(c):
+   l = []
+   b = {}
+   for lines in c:
+      l.append(lines.strip().split())
+   i = 0
+   while i < len(l[1]):
+      b[l[0][i]] = l[1][i]
+      i = i + 1
+   return b
