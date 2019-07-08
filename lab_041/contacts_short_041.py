dd497771ff18db6c50490cc271ef06e08f983fa0
danmccabe00 <52660514+danmccabe00@users.noreply.github.com>
2019-07-08 13:58:58 +0100

    Create contacts_short_041.py

---
+++ lab_041/contacts_short_041.py
@@ -0,0 +1,22 @@
+import sys
+
+a = []
+def contact(l, c):
+      c = c.strip()
+      if c in l:
+         return ("Phone: " + l[c])
+      else:
+         return "No such contact"
+
+def main():
+   d = {}
+   with open(sys.argv[1]) as f:
+      for lines in f:
+         (name, number) = lines.strip().split()
+         d[name] = number
+   for line in sys.stdin:
+      print("Name: " + line.strip())
+      print(contact(d, line))
+
+if __name__ == '__main__':
+   main()
