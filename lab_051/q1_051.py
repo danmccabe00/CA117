e74d19418531696b97d0bdd40ba8f67756772691
danmccabe00 <52660514+danmccabe00@users.noreply.github.com>
2019-07-09 11:37:08 +0100

    Create q1_051.py

---
+++ lab_051/q1_051.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+   l = list(sys.argv[1])
+   i = 1
+   while i < len(l):
+      l[i], l[i - 1] = l[i - 1], l[i]
+      i = i + 2
+   print("".join(l))
+
+if __name__ == '__main__':
+   main()
